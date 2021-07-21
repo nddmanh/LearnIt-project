2c8295a0fcb35ff70d402df786b52dd6620857f1
nddmanh <manhnd.ilec@gmail.com>
2021-07-21 12:57:24 +0700

    [1.0.0] Deploy client to Netlify, fix apiUrl.

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl 
     = process.env.NODE_ENV !== 'production' 
-        ? 'https://learnit-project.herokuapp.com/api'
-        : 'http://localhost:5000/api'
+        ? 'http://localhost:5000/api' 
+        : 'https://learnit-project.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mearn';
 
